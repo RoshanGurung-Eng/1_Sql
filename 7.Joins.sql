b596e3c62c0fa21e1ee1d5322d65ccb0293c6aea
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-10 10:26:04 +0545

    union and functions

--- 7.Joins.sql
+++ 7.Joins.sql
@@ -1,1 +1,51 @@
 -- Joins
+
+select *
+from employee_demographics;
+
+select *
+from employee_salary ;
+
+select *
+from employee_demographics as dem
+inner join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+    ;
+    
+-- outer joins
+
+
+select *
+from employee_demographics as dem
+left join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+    ;
+
+select*
+from employee_demographics as dem
+right join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+    ;
+    
+-- self join
+select emp1.employee_id as emp_santa,
+emp1.first_name as first_name_santa,
+emp1.last_name as last_name_santa,
+emp2.employee_id as emp_name,
+emp2.first_name as first_name_emp,
+emp2.last_name as last_name_emp
+from employee_salary as emp1
+join employee_salary as emp2
+	on emp1.employee_id +1 = emp2.employee_id;
+    
+-- joining multiple tables
+select *
+from employee_demographics as dem
+inner join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+left  join parks_departments as pd
+	ON 	sal.dept_id = pd.department_id
+;
+    
+select *
+from parks_departments;
---
+++ 9.string functions.sql
@@ -0,0 +1,41 @@
+-- string function
+
+select length ('BrockLesnar');
+
+select first_name, length(first_name)
+from employee_demographics
+order by 2;
+
+-- 2.upper
+select upper('hulalala');
+select lower('LOWER');
+
+select first_name, upper(first_name)
+from employee_demographics;
+
+-- 3.Trims, lefttrim and righttrin 
+select rtrim('           sky            ');
+
+-- 4. substring
+select first_name, 
+left(first_name, 4),
+right(first_name, 4),
+substring(first_name,3,2),
+birth_date,
+substring(birth_date,6,2)
+from employee_demographics;
+
+-- 5.replace
+select first_name, replace(first_name,'a','z')
+FROM employee_demographics;
+
+-- 6. Locate
+select locate('o','bobby');
+
+select first_name, locate('an',first_name)
+FROM employee_demographics;
+
+-- 7. Concat
+select first_name, last_name,
+concat (first_name,"",last_name) as fullname
+from employee_demographics;
