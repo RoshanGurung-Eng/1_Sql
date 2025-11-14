a4ac07c84b81c83cfeb70ee6565058d9d1b69db9
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-14 09:31:20 +0545

    cte and temporary_table

--- 13. CTE.sql
+++ 13. CTE.sql
@@ -1,4 +1,6 @@
 -- CTE
+
+-- cte example 
 with CTE_Example as 
 (select gender, avg(salary) as avg_sal, max(salary) max_salary, min(salary) min_salary, count(salary) salary_count
 from employee_demographics as dem
@@ -7,5 +9,45 @@ join employee_salary as sal
 group by gender
 )
 select avg(avg_sal) 
+from CTE_Example
+;
+
+-- subquery examply which is difficult to read
+select avg(avg_sal)
+from (select gender, avg(salary) as avg_sal, max(salary) max_salary, min(salary) min_salary, count(salary) salary_count
+from employee_demographics as dem
+join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+group by gender
+) example_subquery;
+
+-- you can create cte within a cte or multiple cte
+with CTE_Example as 
+(
+select employee_id, gender,birth_date
+from employee_demographics
+where birth_date > "1985-01-01"
+),
+CTE_Example2 as
+(
+select employee_id, salary
+from employee_salary
+where salary > 50000
+)
+select *
+from CTE_Example as e1
+join CTE_Example2 as e2
+	on e1.employee_id = e2.employee_id
+;
+
+-- CTE doesnot need alias we can change the name using parathesis but if use alias while having the name in parenthesis i t will overwrite the alias for eg:
+with CTE_Example (Gender, Avg_sal, MAX_sal, MIN_sal, COUNT_sal) as 
+(select gender, avg(salary) as avg_sal, max(salary) max_salary, min(salary) min_salary, count(salary) salary_count
+from employee_demographics as dem
+join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+group by gender
+)
+select * 
 from CTE_Example
 ;
---
+++ 14. Temporary_Table.sql
@@ -0,0 +1,25 @@
+-- TEMP/ Temporary table
+
+create temporary table Details
+( First_name varchar(50),
+last_name varchar(50),
+fav_actress_movie varchar(100)
+);
+
+insert into Details
+values('Bob', 'Marley', 'OMega');
+
+select * 
+from Details;
+
+-- this is how we gonna use in works:
+
+
+create  temporary table table_over_60k
+select *
+from employee_salary
+where salary >= 60000;
+
+select *
+from table_over_60k
+
