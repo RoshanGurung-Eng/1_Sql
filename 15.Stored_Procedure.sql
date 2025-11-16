2754c789d78a5018e54a53633405b61fdfd40afc
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-16 09:51:09 +0545

    Delimiter

--- 15.Stored_Procedure.sql
+++ 15.Stored_Procedure.sql
@@ -9,3 +9,15 @@ where salary >= 50000;
 
 call large_salaries();
 
+
+Delimiter $$
+create procedure large_salaries2()
+BEGIN
+	select * 
+	from employee_salary
+	where salary >= 50000;
+	select * 
+	from employee_salary
+	where salary >= 70000;
+END $$
+DELIMITER ;
