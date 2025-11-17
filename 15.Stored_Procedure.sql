2ac39ce7e657ce355a95bd59a3b8a9e2b8a3cefc
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-17 20:03:11 +0545

    just_practice

--- 15.Stored_Procedure.sql
+++ 15.Stored_Procedure.sql
@@ -9,7 +9,7 @@ where salary >= 50000;
 
 call large_salaries();
 
-
+-- usecase example pf dilimiter
 Delimiter $$
 create procedure large_salaries2()
 BEGIN
@@ -21,3 +21,15 @@ BEGIN
 	where salary >= 70000;
 END $$
 DELIMITER ;
+call large_salaries2()
+
+Delimiter $$
+create procedure large_salaries3(p_employee_id int)
+BEGIN
+	select salary 
+	from employee_salary
+	where employee_id = p_employee_id;
+	
+END $$
+DELIMITER ;
+call large_salaries3(1);
---
+++ for_Practice.sql
@@ -0,0 +1,4 @@
+-- Practice
+select salary, first_name
+from employee_salary
+where salary <= 70000;
