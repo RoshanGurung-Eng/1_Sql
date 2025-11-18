f063b1234c4f783a4766fa698be557e1dcee2d2a
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-18 18:27:01 +0545

    try data cleaning

--- for_Practice.sql
+++ for_Practice.sql
@@ -2,3 +2,8 @@
 select salary, employee_id, first_name
 from employee_salary
 where salary <= 70000 and employee_id <5;
+
+-- try to clean data
+update employee_salary
+set salary = 0
+where salary is null;
