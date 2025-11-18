aa99ff99ff5cba66b5e809313173ca40cf51a892
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-18 18:31:31 +0545

    about department

--- for_Practice.sql
+++ for_Practice.sql
@@ -3,7 +3,13 @@ select salary, employee_id, first_name
 from employee_salary
 where salary <= 70000 and employee_id <5;
 
--- try to clean data
+-- try to clean data but not able to
 update employee_salary
 set salary = 0
 where salary is null;
+
+-- get to know occupation and department_name
+select occupation , department_name 
+from employee_salary es  
+join parks_departments pd
+where es.dept_id = pd.department_id
