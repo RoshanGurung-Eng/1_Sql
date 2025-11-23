4c00e6f5b26c305d7971911dc08fc93d8068a3d2
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-23 19:19:51 +0545

    End of Trigger

--- 16. Trigger_and_event.sql
+++ 16. Trigger_and_event.sql
@@ -8,3 +8,12 @@ from employee_salary;
 
 delimiter $$
 create trigger employee_insert
+	after insert on employee_salary
+    for each row
+begin
+	insert into employee_demographics (employee_id, first_name, last_name)
+    value (new.employee_id, new.first_name, new.last_name);
+end $$
+
+insert into employee_salary( employee_id, first_name, last_name, occupation, salary, dept_id)
+values(13, 'Robin', 'Hood', "Spy", '100000', null);
