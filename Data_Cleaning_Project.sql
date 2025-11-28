9f0981b6da4498f05910620f4c94d1e165853e9a
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-28 18:04:29 +0545

    data_staging

--- Data_Cleaning_Project.sql
+++ Data_Cleaning_Project.sql
@@ -1,1 +1,20 @@
 -- Project1: Data Cleaning
+
+
+
+-- 1. Remove duplicates
+-- 2. Standardize the data
+-- 3. NUll Values or blank values
+-- 4. remove any coloumns
+
+
+create table layoffs_staging
+like layoffs;
+
+select * 
+from layoffs_staging;
+
+insert layoffs_staging
+select *
+from layoffs;
+
