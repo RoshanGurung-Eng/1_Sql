7be81e4792fe558743e02e9400b13a56e082e901
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-11-30 18:09:33 +0545

    cleaning

--- Data_Cleaning_Project.sql
+++ Data_Cleaning_Project.sql
@@ -27,6 +27,31 @@ select *
 from duplicate_CTE 
 WHERE row_num > 1;
 
-select * 
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `total_laid_off` text,
+  `date` text,
+  `percentage_laid_off` text,
+  `industry` text,
+  `source` text,
+  `stage` text,
+  `funds_raised` int DEFAULT NULL,
+  `country` text,
+  `date_added` text,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+insert layoffs_staging2
+select *
+from layoffs;
+
+with duplicate_cte as (
+select *,
+row_number() over(partition by company, location, industry, total_laid_off, percentage_laid_off, `date`, country, funds_raised ) as row_num
 from layoffs_staging
-where company = '2U';
+)
+select *
+from duplicate_CTE 
+WHERE row_num > 1;
+
