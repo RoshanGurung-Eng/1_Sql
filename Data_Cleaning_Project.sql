91667139d83ee291c723733085f7c017dbb0ea9f
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-12-04 19:26:40 +0545

    null or blanck space

--- Data_Cleaning_Project.sql
+++ Data_Cleaning_Project.sql
@@ -96,6 +96,31 @@ modify column `date` DATE;
 -- working with null and blank values
 
 select * 
-from layoffs_staging2;
+from layoffs_staging2 
+where total_laid_off IS NULL
+and percentage_laid_off IS NULL;
 
+select *
+from layoffs_staging2
+where industry is null
+or industry = '';
+
+select *
+from layoffs_staging2
+where company = 'Appsmith'
+;
+
+select t1.industry, t2.industry 
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+where (t1.industry is null or t1.industry = '')
+and t2.industry is not null;
+
+update layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+set t1.industry = t2.industry
+where (t1.industry is null or t1.industry = '')
+and t2.industry is not null;
 
