c137ff43abd110a605754ca9d26a349dc976bdda
RoshanGurung-Eng <roshantheaomine@gmail.com>
2025-12-08 22:01:48 +0545

    prqactice having

--- 5.having vs where.sql
+++ 5.having vs where.sql
@@ -4,6 +4,7 @@ from employee_demographics
 group by gender
 having  avg(age) > 40
 ;
+
 select occupation, avg(salary)
 from employee_salary
 where occupation like "%manager%"
@@ -11,3 +12,9 @@ group by occupation
 having avg(salary) > 75000
 ;
 
+select occupation , avg(salary)
+from employee_salary
+where occupation like "%manager"
+group by occupation
+having avg(salary) > 56000
+;
