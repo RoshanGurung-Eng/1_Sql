f1e9dc52f79aa3e2ae03905874c51eb0c03ca765
RoshanGurung-Eng <roshantheaomine@gmail.com>
2026-01-11 23:35:13 +0545

    subqueiries practiseing

--- 11.Subqueries.sql
+++ 11.Subqueries.sql
@@ -25,4 +25,14 @@ select avg(avg_age), avg(max_age), avg(min_age), avg(count_age)
 from 
 (select gender, avg(age) as avg_age, max(age) as max_age, min(age) as min_age, count(age) as count_age
 from employee_demographics
-group by gender) as Agg_table
+group by gender) as Agg_table;
+
+-- practise
+select *
+from employee_demographics
+where employee_id in 
+		(select employee_id
+        from employee_salary
+        where dept_id =1)
+;
+
