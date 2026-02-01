05e020485aec24def20a467dcca1b0b0266dd995
RoshanGurung-Eng <roshantheaomine@gmail.com>
2026-02-01 11:39:14 +0545

    limit

--- 6. limit_and_aliasing.sql
+++ 6. limit_and_aliasing.sql
@@ -11,3 +11,9 @@ select gender, avg(age) as avg_age
 from employee_demographics
 group by gender
 having avg_age > 40;
+
+-- practice 
+select *
+from employee_demographics
+order by age desc
+limit 2, 1 ;
