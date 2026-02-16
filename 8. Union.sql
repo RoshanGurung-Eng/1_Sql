47f9f7d954d9394122bcff420bad06ba87ce8566
RoshanGurung-Eng <roshantheaomine@gmail.com>
2026-02-16 17:32:55 +0545

    Union

--- 8. Union.sql
+++ 8. Union.sql
@@ -27,3 +27,16 @@ from employee_demographics
 union all
 select first_name
 from employee_salary;
+
+select first_name,last_name, 'Old Man' as label 
+from employee_demographics
+where age >40 and gender = "Male"
+union
+select first_name,last_name, 'Old Lady' as label 
+from employee_demographics
+where age >40 and gender = "Female"
+union
+select first_name, last_name, 'Highly paid employee' as label
+from employee_salary
+where salary>70000
+order by first_name, last_name;
