d6e1543580268eceaf2330eba34e3f57d88e0f05
RoshanGurung-Eng <roshantheaomine@gmail.com>
2026-02-18 09:50:35 +0545

    CTE_Practice

--- for_Practice.sql
+++ for_Practice.sql
@@ -57,7 +57,18 @@ from employee_demographics as ed
 inner join
 	employee_salary as es on ed.employee_id = es.employee_id
   ;  
-    
+-- cte practice:
+with CTE_Example as 
+(select gender, avg(salary) as avg_sal, max(salary) max_salary, min(salary) min_salary, count(salary) salary_count
+from employee_demographics as dem
+join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+group by gender
+)
+select avg(avg_sal) 
+from CTE_Example
+;
+
 -- details of employee
 select count(*)
 from employee_demographics;
